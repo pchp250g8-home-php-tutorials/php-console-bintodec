fc6909c2c42535da7f42543f5959170243be8509
Alexander <pchp250g8@tutamail.com>
2026-04-13 14:48:30 +0300

    Fixed Source Code. Foxed ReadMe

--- BinToDec.php
+++ BinToDec.php
@@ -2,7 +2,7 @@
     const MAX_INT = 2 ** 32 - 1;
     $nDecNum = 0;
     $nBinPower = 1;
-    $nMaxBinDigits = floor(log(MAX_INT, 2));
+    $nMaxBinDigits = ceil(log(MAX_INT, 2));
     echo("Input a binary number:\r\n");
     $strLine = chop(fgets(STDIN));
     $nStrLen = strlen($strLine);
@@ -20,6 +20,6 @@
         $nDecNum += ($nBinDight * $nBinPower);
         $nBinPower *= 2;
     }
-    printf("The decenary equivalent of the binary number %s is %d\r\n", $strLine,$nDecNum);
+    printf("The decimal equivalent of the binary number %s is %d\r\n", $strLine, $nDecNum);
     fgetc(STDIN);
 ?>
